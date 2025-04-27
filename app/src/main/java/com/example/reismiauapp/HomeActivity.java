4cec8ecc66cb50b4ca8357aa452acb07361140ff
VanessaVanzin <vanessabvanzin@hotmail.com>
2025-04-27 17:48:51 -0300

    feat: continuação home

--- app/src/main/java/com/example/reismiauapp/HomeActivity.java
+++ app/src/main/java/com/example/reismiauapp/HomeActivity.java
@@ -1,4 +1,36 @@
 package com.example.reismiauapp;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView instagramIcon = findViewById(R.id.instagram_icon);
+
+        instagramIcon.setOnClickListener(v -> {
+            String instagramUrl = "https://www.instagram.com/reismiau/";
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(instagramUrl));
+            startActivity(intent);
+        });
+    }
 }
