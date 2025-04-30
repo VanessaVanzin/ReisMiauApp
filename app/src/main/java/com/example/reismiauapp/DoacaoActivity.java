d32f9c41fe4711c8e3376eb93b3ac36e4bcc3e87
VanessaVanzin <vanessabvanzin@hotmail.com>
2025-04-30 18:33:31 -0300

    feat: finalização tela doação

--- app/src/main/java/com/example/reismiauapp/DoacaoActivity.java
+++ app/src/main/java/com/example/reismiauapp/DoacaoActivity.java
@@ -1,9 +1,14 @@
 package com.example.reismiauapp;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,13 +31,44 @@ public class DoacaoActivity extends AppCompatActivity {
             return insets;
         });
 
+        Button btnSejaParceiro = findViewById(R.id.doacao_button);
+        Button btnCopiarPix = findViewById(R.id.btn_copiar_chave_pix);
         ImageView instagramIcon = findViewById(R.id.ic_instagram);
+        ImageView whatsappIcon = findViewById(R.id.ic_whatsapp);
         ImageView emailIcon = findViewById(R.id.ic_mail);
+
+        btnSejaParceiro.setOnClickListener(v -> {
+            String phoneNumber = "+5554991202991";
+            String message = "Olá! Quero ser parceiro da Reis Miau.";
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + message));
+            startActivity(intent);
+        });
+
+        btnCopiarPix.setOnClickListener(v -> {
+            String chavePix = "00020126330014br.gov.bcb.pix0111008414990635204000053039865802BR5924SUELEN MARTINS GRIEBELER6015FLORES DA CUNHA62120508ReisMiau6304EC74";
+
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("Chave Pix", chavePix);
+            clipboard.setPrimaryClip(clip);
+
+            Toast.makeText(this, "Chave Pix copiada com sucesso!", Toast.LENGTH_SHORT).show();
+        });
 
         instagramIcon.setOnClickListener(v -> {
             String instagramUrl = "https://www.instagram.com/reismiau/";
 
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(instagramUrl));
+            startActivity(intent);
+        });
+
+        whatsappIcon.setOnClickListener(v -> {
+            String phoneNumber = "+5554991202991";
+            String message = "Olá! É da Reis Miau?";
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + message));
             startActivity(intent);
         });
 
@@ -44,8 +80,11 @@ public class DoacaoActivity extends AppCompatActivity {
 
             if (emailIntent.resolveActivity(getPackageManager()) != null) {
                 startActivity(emailIntent);
+            } else {
+                Toast.makeText(this, "Você precisa ter um aplicativo de email instalado!", Toast.LENGTH_LONG).show();
             }
         });
+
 
         BottomNavBarHelper.setup(this);
     }
