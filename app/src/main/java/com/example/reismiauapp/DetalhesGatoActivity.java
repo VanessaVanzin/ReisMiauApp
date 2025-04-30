219f1a2f8f76a73db0298e4671c6c4d8836ca942
VanessaVanzin <vanessabvanzin@hotmail.com>
2025-04-30 18:46:26 -0300

    feat: ajuste checkbox e finalização tela detalhes gato

--- app/src/main/java/com/example/reismiauapp/DetalhesGatoActivity.java
+++ app/src/main/java/com/example/reismiauapp/DetalhesGatoActivity.java
@@ -1,7 +1,9 @@
 package com.example.reismiauapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -26,12 +28,25 @@ public class DetalhesGatoActivity extends AppCompatActivity {
         });
 
         ImageButton btnEditar = findViewById(R.id.btnEditar);
+        Button btnAdotar = findViewById(R.id.btnAdotar);
+        ImageButton btnVoltar = findViewById(R.id.btnVoltar);
+
         btnEditar.setOnClickListener(v -> {
             Intent intent = new Intent(DetalhesGatoActivity.this, CadastroAdminActivity.class);
             startActivity(intent);
         });
 
-        ImageButton btnVoltar = findViewById(R.id.btnVoltar);
+        btnAdotar.setOnClickListener(v -> {
+            String petName = "William";
+            String phoneNumber = "+5554992223853";
+            String message = "Olá! Acessei o app e tenho interesse em adotar o seguinte pet: \n" + petName;
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + Uri.encode(message)));
+            startActivity(intent);
+        });
+
+
         btnVoltar.setOnClickListener(v -> {
             finish();
         });
